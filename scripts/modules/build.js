ef7acc64fc4e4b7d94aac32a9fced256b0393b03
Jonathan Lo <jonathanlo411@gmail.com>
2022-12-15 17:38:46 -0800

    improve search // fuse not implemented into base

--- scripts/modules/build.js
+++ scripts/modules/build.js
@@ -1,5 +1,6 @@
+import Fuse from './fuse6.6.2.js'
 
-function buildHTML(source, data) {
+function buildHTML(source, data, targetTitle) {
 
     // Obtain targets
     const domTarget = document.getElementsByClassName('leftside')[0];
@@ -7,12 +8,12 @@ function buildHTML(source, data) {
     let malWatchButton = document.getElementById('broadcast-block')
     var htmlButton;
     
-    // Check if data is available
+    // Check if d*ata is available
     if (!data || (data.msg && data.msg === 'invalid')) { source = "Unavailable" }
 
     // Selectively build buttons
     if (source === "MangaDex") {
-        const mangaID = data['data'][0]['id'];
+        const mangaID = removeMDNonOfficial(data)[0].id //searchData(data, targetTitle, "json");
         htmlButton = createButton('MangaDex', `https://mangadex.org/title/${mangaID}`)
     } else if (source === "GoGoAnime") {
         const url = scrapeGoGoAnime(data)
@@ -67,6 +68,37 @@ function scrapeGoGoAnime(stringHTML) {
         }
     }
     return `https://ww3.gogoanime2.org/${animeLink.replace("https://myanimelist.net/", "")}`
+}
+
+// Searches through json to find optimal result; Uses Fuse
+function searchData(data, targetTitle, dataType) {
+    if (dataType === "json") {
+        const constraints = { includeScore: true, keys: [ "attributes.title.en" ] }
+        const fuse = new Fuse(data['data'], constraints)
+        const result = fuse.search(targetTitle)
+        console.log(result)
+        return (result.length === 0) ? data['data'][0].id : result[0].item.id;
+    }
+}
+
+// Removes any data that has one of the banned tags
+function removeMDNonOfficial(data) {
+    const bannedTags = ["Oneshot", "Doujinshi"];
+    var approvedData = [];
+    var i;
+    for (i = 0; i < data['data'].length; i ++) {
+        var j;
+        var notBanned = true;
+        var dataEntry = data['data'][i]
+        for (j = 0; j < dataEntry["attributes"]["tags"].length; j ++) {
+            if (bannedTags.includes(dataEntry["attributes"]["tags"][j]["attributes"]["name"]["en"])) {
+                notBanned = false;
+                break;
+            }
+        }
+        if (notBanned) { approvedData.push(data['data'][i]) };
+    }
+    return approvedData;
 }
 
 export default buildHTML
